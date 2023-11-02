8132e02272dba511de7265a7bf3d66e10281751e
김창준 <kchang6869@gmail.com>
2023-11-02 17:18:27 +0900

    :bug: 미들웨어 rewrite에서 redirect로 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export default function middleware(request: NextRequest) {
+  console.log(request.nextUrl.pathname);
   if (request.nextUrl.pathname === '/posts' || request.nextUrl.pathname === '/posts/pages') {
-    return NextResponse.rewrite(new URL(`/posts/pages/1`, request.url));
+    return NextResponse.redirect(new URL(`/posts/pages/1`, request.url));
   }
   return NextResponse.next();
 }
