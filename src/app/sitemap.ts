880251c24dc381d54b88a320c70492dee9f11c9d
June <godan6869@naver.com>
2023-11-23 17:07:28 +0900

    :pencil2: Sitemap 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,6 @@ import siteConfig from '@/site.config';
 import { allPosts } from '@/contentlayer/generated';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const allCategory = ['all', 'dev', 'life', 'fire'];
   return [
     {
       url: siteConfig.url,
@@ -16,21 +15,10 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.5,
     },
-    {
-      url: `${siteConfig.url}/posts/pages/1`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
     ...allPosts.map((post) => {
       return {
         url: `${siteConfig.url}/posts/${post._raw.flattenedPath}`,
         lastModified: new Date(post.createdAt),
-      };
-    }),
-    ...allCategory.map((category) => {
-      return {
-        url: `${siteConfig.url}/posts/categories/${category}/pages/1`,
       };
     }),
   ];
