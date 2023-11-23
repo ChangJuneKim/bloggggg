5693c1f2f54bbbbed36313b5f48b66cea4c11fa9
June <godan6869@naver.com>
2023-11-23 21:08:24 +0900

    .

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,6 +1,5 @@
 // Route segment config
 import { ImageResponse } from 'next/server';
-import OpenGraph from '@/components/OpenGraph';
 
 export const runtime = 'edge';
 
@@ -17,11 +16,92 @@ export const contentType = 'image/png';
 export default async function OpengraphImage() {
   return new ImageResponse(
     (
-      <OpenGraph
-        title="김창준의 블로그입니다."
-        tags={['개발', '일상', '삽질']}
-        url="https://www.changjune.com/"
-      />
+      <>
+        <img
+          alt={'ㅁㄴㅇ'}
+          src={'https://changjune.com/assets/images/banner/home.webp'}
+          width={size.width}
+          height={size.height}
+          style={{
+            width: `${size.width}px`,
+            height: `${size.height}px`,
+            objectFit: 'cover',
+            filter: 'brightness(80%)',
+            backgroundColor: 'rgba(0, 0, 255, 0.2)',
+          }}
+        />
+        <span
+          style={{
+            position: 'absolute',
+            top: '25%',
+            left: '80px',
+            width: `${OpenGraphImageSize.width - 600}px`,
+            wordWrap: 'break-word',
+            wordBreak: 'break-all',
+            color: 'white',
+            fontSize: '50px',
+            fontWeight: 'bold',
+            textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+            display: '-webkit-box',
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: 'vertical',
+            overflow: 'hidden',
+          }}
+        >
+          반갑습니다
+        </span>
+        <span
+          style={{
+            position: 'absolute',
+            top: '65%',
+            left: '80px',
+            color: 'white',
+            fontSize: '40px',
+            fontWeight: 'bold',
+            textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+            textAlign: 'center',
+          }}
+        >
+          #tag
+        </span>
+        <span
+          style={{
+            position: 'absolute',
+            top: '80%',
+            left: '80px',
+            color: 'white',
+            fontSize: '28px',
+            fontWeight: 'bold',
+            textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+            textAlign: 'center',
+          }}
+        >
+          테스트 url
+        </span>
+        <div
+          style={{
+            position: 'absolute',
+            top: '65%',
+            right: '5%',
+            borderRadius: '8px',
+            overflow: 'hidden',
+            filter: 'brightness(0.95)',
+          }}
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            alt={'로고'}
+            src={'https://changjune.com/assets/images/resume/logo.png'}
+            width={1300}
+            height={630}
+            style={{
+              width: `100px`,
+              height: `100px`,
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+      </>
     ),
     // ImageResponse options
     {
